3a19e0cbaa2503fd48b2d92a0f32150ba5b3ed19
shift101260 <shift101260@gmail.com>
2026-08-30 18:52:35 +0800

    Create unregisteredBuilding.js

---
+++ js/modules/unregisteredBuilding.js
@@ -0,0 +1,2 @@
+window.openUnregisteredBuildingModal = () => document.getElementById('unregistered-building-modal').classList.remove('hidden');
+window.closeUnregisteredBuildingModal = () => document.getElementById('unregistered-building-modal').classList.add('hidden');
