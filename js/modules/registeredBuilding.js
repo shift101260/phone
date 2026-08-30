5a16ad78b4ab6d4e2bb9ecc2dbe3be10a48fe7c2
shift101260 <shift101260@gmail.com>
2026-08-30 18:52:13 +0800

    Create registeredBuilding.js

---
+++ js/modules/registeredBuilding.js
@@ -0,0 +1,2 @@
+window.openRegisteredBuildingModal = () => document.getElementById('registered-building-modal').classList.remove('hidden');
+window.closeRegisteredBuildingModal = () => document.getElementById('registered-building-modal').classList.add('hidden');
