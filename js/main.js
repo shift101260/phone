1ceb4510229ad9d68c9b045ccbab8d376a0ed923
shift101260 <shift101260@gmail.com>
2026-08-30 18:57:10 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// js/main.js
+import './modules/landBasic.js';
+import './modules/registeredBuilding.js';
+import './modules/unregisteredBuilding.js';
+import './modules/land-change-calc.js';
+import './modules/payment-calc.js';
+import './modules/solar-calc.js';
+
+console.log('Ruili 系統主程式 (main.js) 與所有模組已成功載入！');
