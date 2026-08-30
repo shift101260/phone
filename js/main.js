6912eca6e8b0813918eb388ecf48956f36031afd
shift101260 <shift101260@gmail.com>
2026-08-30 21:20:58 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,5 @@ import './modules/land-change-calc.js';
 import './modules/payment-calc.js';
 import './modules/solar-calc.js';
 import './modules/case-edit.js';
-import './modules/case-edit.js';
 
 console.log('Ruili 系統主程式 (main.js) 與所有模組已成功載入！');
