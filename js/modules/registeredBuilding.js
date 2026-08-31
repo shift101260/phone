a88ed28d9be08cfadc2905ea4ef091bb465e924d
shift101260 <shift101260@gmail.com>
2026-09-01 01:58:03 +0800

    Update registeredBuilding.js

--- js/modules/registeredBuilding.js
+++ js/modules/registeredBuilding.js
@@ -1,9 +1,8 @@
-// js/modules/registered-building.js
 window.openRegisteredBuildingModal = function() {
-    const modal = document.getElementById('editCaseModal');
+    const modal = document.getElementById('registered-building-modal');
     if (modal) modal.classList.remove('hidden');
 };
 window.closeRegisteredBuildingModal = function() {
-    const modal = document.getElementById('editCaseModal');
+    const modal = document.getElementById('registered-building-modal');
     if (modal) modal.classList.add('hidden');
 };
